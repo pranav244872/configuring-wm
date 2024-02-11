456f50afc430f558096d12ee35ac85354a038341
MrLuffyMom <137860111+pranav244872@users.noreply.github.com>
2024-02-11 16:50:43 +0530

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,68 +1,42 @@
 #!/bin/bash
 
 # Define the list of packages to install
-packages=(
-    git
-    firefox
-    bluez
-    bluez-utils
-    networkmanager
-    hyprland
-    kitty
-    mako
-    wireplumber
-    polkit-kde-agent
-    waybar
-    gtk3
-    less
-    file
-    qt5-wayland
-    lf
-    neovim
-    wl-clipboard
-    rofi
-    udiskie
-    python-pywal
-    swaybg
-    papirus-icon-theme
-    noto-fonts
-    noto-fonts-emoji
-    noto-fonts-cjk
-    ttf-jetbrains-mono-nerd
-  )
-sudo pacman -Syu --noconfirm "${packages[@]}
+sudo pacman -S --noconfirm git firefox bluez bluez-utils networkmanager hyprland kitty mako wireplumber polkit-kde-agent waybar gtk3
+sudo pacman -S --noconfirm less file qt5-wayland lf neovim wl-clipboard rofi udiskie python-pywal swaybg papirus-icon-theme noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono-nerd
 
 # Create a directory called 'del' in the home directory
 mkdir ~/del
-
+echo "Created a directory called del in home"
 
 # Create Downloads folder in the home directory
 mkdir -p ~/Downloads
+echo "Created a directory called downloads"
 
 # Create Pictures folder in the home directory
 mkdir -p ~/Pictures
+echo "Created a directory called Pictures"
 
 # Create Wallpapers folder within Pictures
 mkdir -p ~/Pictures/Wallpapers
-
+echo "Created a directory called Pictures --> Wallpapers"
 # Creating Screenshots folder within Pictures
 mkdir -p ~/Pictures/Screenshots
-
+echo "Created a directory called Pictures --> Screenshot"
 # Change directory to 'del'
 cd ~/del || exit
-
+echo "pwd --> del"
 # Clone the Yay AUR helper repository
 git clone https://aur.archlinux.org/yay.git
-
+echo "cloning yay.git repository"
 # Change directory to 'yay'
 cd yay || exit
-
+echo "pwd --> ~/del/yay"
 # Build and install Yay using makepkg
+echo "building yay"
 makepkg -si
-
 # Enable and start Pipewire and WirePlumber services using systemctl
-systemctl --user --now enable pipewire wireplumber
-
+echo "Enable and start Pipewire and WirePlumber services using systemctl"
+sudo systemctl --user --now enable pipewire wireplumber
 # Check if yay is installed
 if ! command -v yay &> /dev/null; then
     echo "yay is not installed. Please install yay first."
@@ -70,32 +44,35 @@ if ! command -v yay &> /dev/null; then
 fi
 
 # Install hyprshot using yay
+echo "installing hyprshot for screenshots"
 yay -S hyprshot
 
 # Go to ~/del directory
+echo "pwd ~/del"
 cd ~/del || exit
 
 # Clone the repository
+echo "cloning pranav's repository"
 git clone https://github.com/pranav244872/configuring-wm.git || exit
 
 # Move into the repository directory
 cd configuring-wm || exit
+echo "pwd ~/del/configuring-wm"
 
 # Copy .bashrc from the repository to the home directory
 cp ~/del/configuring-wm/.bashrc ~/.bashrc
-
+echo "copied .bashrc from pranavs repo to system repo"
 # Display a message indicating success
 echo "Successfully copied .bashrc from configuring-wm to ~/.bashrc"
 
 # Making a scripts folder in home
 mkdir -p ~/scripts
-
+echo "Successfully created a scripts folder"
 # Copying the scripts folder from configuring-wm to ~/scripts
 cp ~/del/configuring-wm/colors.sh ~/scripts/
 
 # Making config files
 # Source directory
-dir_a="# Source directory
 dir_a="~/del/configuring-wm/.config"
 # Destination directory
 dir_b="~/.config"
@@ -116,3 +93,4 @@ for folder in "$dir_a"/*; do
 done
 
 echo "Folders copied successfully."
+echo "succesfully copied dotfiles"
