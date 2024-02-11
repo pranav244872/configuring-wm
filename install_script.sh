9dee7d0479a2662338a2b0f879417a35e461903c
MrLuffyMom <137860111+pranav244872@users.noreply.github.com>
2024-02-11 19:07:44 +0530

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -3,7 +3,7 @@
 sudo pacman -S --noconfirm git firefox bluez bluez-utils networkmanager hyprland kitty mako polkit-kde-agent waybar gtk3
 sudo pacman -S --noconfirm less file qt5-wayland lf neovim wl-clipboard rofi udiskie python-pywal swaybg papirus-icon-theme noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono-nerd
 sudo pacman -S --noconfirm xdg-desktop-portal-hyprland blueman
-sudo pacman -S --noconfirm qt6-wayland
+sudo pacman -S --noconfirm qt6-wayland otf-font-awesome
 # Create a directory called 'del' in the home directory
 mkdir ~/del
 echo "Created a directory called del in home"
@@ -73,7 +73,8 @@ chmod +x colors.sh
 echo "gave executable permissions to colors.sh script"
 
 cp -r ~/del/configuring-wm/.config/* ~/.config/
-
-sudo systemctl enable bluetooth.service
 echo "Folders copied successfully."
 echo "succesfully copied dotfiles"
+
+sudo systemctl enable bluetooth.service
+echo "enabled bluetooth service"
