791a0c54fb9fc0912958f61d79785d3ed13970e1
pranav244872 <aekbotepranav25@gmail.com>
2026-06-30 23:12:59 +0530

    disable sshd on boot by default

--- script.d/50-services.sh
+++ script.d/50-services.sh
@@ -2,5 +2,6 @@ enable_services() {
   section "Systemd services"
   systemctl --user daemon-reload
   sudo systemctl enable --now bluetooth
+  sudo systemctl disable sshd 2>&1 | tail -1
   systemctl --user enable swayosd-server 2>&1 | tail -1
 }
