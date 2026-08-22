2afc981793bc9bbcba9271d8e42ac1419c19d979
pranav244872 <aekbotepranav25@gmail.com>
2026-08-22 21:11:28 +0530

    ripgrep: shared ignore config; point fzf-lua at it

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -5,6 +5,9 @@ require("fzf-lua").setup({
     previewer = "codeaction",  -- Clean built-in previewer
     async_or_timeout = 5000,
   },
+  -- fzf-lua nullifies the ambient rg config unless given explicitly,
+  -- so point it at our shared ignore rules (__pycache__, venvs, ...)
+  RIPGREP_CONFIG_PATH = "~/.config/ripgrep/config",
 })
 
 require("fzf-lua").register_ui_select()
